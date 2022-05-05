9bfd92d7a1a1c7bee20c4d5ad1f52d21c6247f66
foodisdabomb <100959223+foodisdabomb@users.noreply.github.com>
2022-05-05 00:10:04 -0500

    Update pygame_day_1.py
    good thing my get hyped playlist kept me sane... wait comments???????????
    
    also had NO IDEA that this was due TOMORROW??? Procrastination stinks
    and it's kind of messy because I had to draw somethings.
    Anyways I have 2 hours to complete the math o_o oops

--- pygame_day_1.py
+++ pygame_day_1.py
@@ -14,16 +14,58 @@ tutorial = False
 fades = 0
 runfades = False
 textfadedone = False
-def on_mouse_down(pos): #button configuration 
+tutorialtextline = 1
+tutorialtexts = ""
+tutorial_file = open("tutorial.txt", "r")
+listoflines = tutorial_file.readlines()
+line = 0
+characterpos = 50
+stupidscreen = Actor("stupid.png", pos = (0,700))
+linecasted = False
+mathisinsession = False
+
+#do i use actors for everything???maybe i make the math thing a class
+#various actor creations
+#cannot figure out how to make it simplified to ignore this
+base = Actor("base.png", pos = (750,200))
+one = Actor("1.png", pos = (600,220))
+two = Actor("2.png", pos = (650,220))
+three = Actor("3.png", pos = (700, 220))
+four = Actor("4.png", pos = (750,220))
+five = Actor("5.png", pos = (800,220))
+six = Actor("6.png", pos = (600,260))
+seven = Actor("7.png", pos = (650,260))
+eight = Actor("8.png", pos = (700,260))
+nine = Actor("9.png", pos = (750,260))
+zero = Actor("0.png", pos = (800,260))
+backspace = Actor("back.png", pos = (675, 300))
+submit = Actor("submit.png", pos = (725, 300))
+
+def on_mouse_down(pos, button): #button configuration 
   #calculates the distances for the mouse and the button
-  global loading
+  global loading, tutorial, line, tutorialtexts, mathisinsession, linecasted
   startdistancesx = pos[0] - 575 
   startdistancesy = pos[1]- 335
-  
-  if -190 < startdistancesx < 190 and  -40 < startdistancesy < 40: #if the user clicks the start button
-    loading = True
-  elif -190 < startdistancesx < 190 and  55 < startdistancesy < 130: #if the user clicks the settings
-    print(str(startdistancesx) + " " + str(startdistancesy))
+  if tutorial == False and button == mouse.LEFT:
+    if -190 < startdistancesx < 190 and  -40 < startdistancesy < 40: #if the user clicks the start button
+      loading = True
+    elif -190 < startdistancesx < 190 and  55 < startdistancesy < 130: #if the user clicks the settings
+      print(str(startdistancesx) + " " + str(startdistancesy))
+  if tutorial == True and button == mouse.LEFT and line < 8:
+    
+    if line == 2:
+      linecasted = True
+      line = 7
+      clock.schedule_unique(mathzzzz,5)
+    tutorialtext()
+  if mathisinsession == True and button == mouse.RIGHT:
+    tutorialtext()
+  if 10 <= line <= 12:
+    tutorialtext()
+    
+
+
+
 
 def draw_rect_alpha(surface, color, rect):
     shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
@@ -53,6 +95,7 @@ def loadingscreen():
 
 
 
+
 def fadess():
   global tutorial, fades, runfades, textfadedone
   runfades = True
@@ -64,18 +107,30 @@ def fadess():
         fades = 1
     if fades == 1:
       textfadedone = True
+      
 
 
 
 
 
-
+      
+#----------------
+def tutorialtext():
+  global line, listoflines, tutorialtexts
   
+  tutorialtexts = listoflines[line]
+  line += 1
+
+
+
+
+
+#--------------------------  
 def draw():
-  global loading, runfades, textfadedone
+  global loading, runfades, textfadedone, linecasted, mathisinsession
   if tutorial == False:
     screen.blit("hello.png", (0,0))
- 
+
   if loading == True:
     clock.schedule_unique(loadingscreen, 1)
   if tutorial == True:
@@ -84,27 +139,68 @@ def draw():
       clock.schedule_unique(fadess, 3)
     if runfades == True and textfadedone == False:
       fadess()
+      
     if textfadedone == True:
-      draw_rect_alpha(surface, (0, 0, 255), (0, 500, WIDTH, HEIGHT))
-      draw_rect_alpha(surface, (255,255,255), (0,485,WIDTH, 15))
+      screen.blit("fish.png", (-100,350))
+      if line >= 1:
+        draw_rect_alpha(surface, (0, 0, 255), (0, 500, WIDTH, HEIGHT))
+        draw_rect_alpha(surface, (255,255,255), (0,485,WIDTH, 15))
+        screen.draw.text(tutorialtexts, topleft = (40,500), color = (255,255,255), fontsize = 35)
+      if linecasted == True:
+        screen.blit("bobber.png", (WIDTH/2,375))
+        if mathisinsession == True:
+          screen.blit("exclamation.png", (500,200))
+      if line >= 8:
+        draw_rect_alpha(surface, (0, 0, 255), (0, 500, WIDTH, HEIGHT))
+        draw_rect_alpha(surface, (255,255,255), (0,485,WIDTH, 15))
+        screen.draw.text(tutorialtexts, topleft = (40,500), color = (255,255,255), fontsize = 35)
+      
     
+def mathzzzz():
+  global mathisinsession, line
+  mathisinsession = True
+  line = 8
+  tutorialtext()
 
-
-
-
+def drawmaths():
+    base.draw()
+    one.draw()
+    two.draw()
+    three.draw()
+    four.draw()
+    five.draw()
+    six.draw()
+    seven.draw()
+    eight.draw()
+    nine.draw()
+    zero.draw()
+    backspace.draw()
+    submit.draw()
 pgzrun.go()
 
 
 
 #todo list
 
-#figure out how to fade to a tutorial and make it like a letter by letter appearance
+
 #figure out how to make the math box pop up in random places
-#figure out how to let the user control the fishing rod
+
 #figure out how to catch fish
 #figure out how to do time to see how long their average was to figure out each question
-#is that it?
+
 #figure out how to make the math problems more difficult
 #make the math problems like a popup ad where its like a blue screen and then math and depending on the mode, they can click the number buttons? should they type it in?
-#figure out how to show the sky getting like night time
+
 #just make a tutorial ig
+
+
+
+#todo
+
+
+#math popup...make the buttons
+#make the fish graphic
+#6 minute timer
+#make a new function for fishing recreate tutorial
+#do the display screen
+#DONE
